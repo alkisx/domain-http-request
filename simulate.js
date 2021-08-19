d4bbb1760205b9f2288e743e243208ad23a12242
Alkiviadis Doulgeris <adoulgeris@sqlearn.com>
2021-08-19 18:08:50 +0300

    add simulate file

---
+++ simulate.js
@@ -0,0 +1,13 @@
+import {Request, Config} from "./index.js";
+
+
+const cfg = {
+    'url': 'http://tms.local',
+    'path': '/api'
+};
+
+const req = new Request(cfg);
+
+
+
+console.log('Finished');

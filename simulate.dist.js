8f9158b245ae62d02bdcb544b452d4883eeefc0a
Alkiviadis Doulgeris <adoulgeris@sqlearn.com>
2021-08-20 18:52:08 +0300

    add example of setting decompression to off in case of problems with compressed responses

--- simulate.dist.js
+++ simulate.dist.js
@@ -3,7 +3,10 @@ import {Request, Config} from "./index.js";
 
 const cfg = {
     'url': 'http://tms.local',
-    'path': '/api'
+    'path': '/api',
+    options:{
+        decompress: false
+    }
 };
 
 const req = new Request(cfg);
